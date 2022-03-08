0ca2b8c3336158d8fa04576d3d98ae60593ac818
Ivan Kuchin <ivan.kuchin@gmail.com>
2022-03-07 23:15:56 -0500

    convert comment fields to type text

---
+++ 2022030801_varchar_to_text.up.sql
@@ -0,0 +1,6 @@
+ALTER TABLE `medical_records`
+CHANGE `___another_organ_resect___other___another_option` `___another_organ_resect___other___another_option` text COLLATE 'utf8_general_ci' NOT NULL AFTER `___another_organ_resect___other`;
+ALTER TABLE `medical_records`
+CHANGE `___other___another_option` `___other___another_option` text COLLATE 'utf8_general_ci' NOT NULL AFTER `___other`;
+ALTER TABLE `medical_records`
+CHANGE `___relapse_metastases___other___another_option` `___relapse_metastases___other___another_option` text COLLATE 'utf8_general_ci' NOT NULL AFTER `___relapse_metastases___other`;
